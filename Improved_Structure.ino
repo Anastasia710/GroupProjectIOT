f33fb55552094d94d910e3d51916951701bcbb65
nameThatIsAUsername <45175573+nameThatIsAUsername@users.noreply.github.com>
2018-11-26 10:37:59 -0700

    Update Improved_Structure.ino

--- Improved_Structure.ino
+++ Improved_Structure.ino
@@ -11,7 +11,7 @@ unsigned long currentTime;  //since last action
 int ledAmount = 4;
 int ledPinNum[ledAmount] = {10, 11, 12, 13};
 int buttonPinNum[ledAmount] = {2, 3, 4, 5};  
-//volatile bool buttonState[ledAmount] = //match up with buttonPin inputs; put in different function?
+volatile int buttonState[]; //match up with buttonPin inputs; put in different function?
  for (int i = 0; i < ledAmount; ++i)
   {
    pinMode(ledPinNum[i], OUTPUT);   //LED's are on pins 10, 11, 12, 13
@@ -19,25 +19,31 @@ int buttonPinNum[ledAmount] = {2, 3, 4, 5};
   }
 
 //set up interrupts
- attachInterrupt(digitalPinToInterrupt(buttonPinNum[0]), firstLight, CHANGE);
- attachInterrupt(digitalPinToInterrupt(buttonPinNum[1]), secondLight, CHANGE);
+ attachInterrupt(digitalPinToInterrupt(buttonPinNum[0]), firstLight, FALLNG);
+ attachInterrupt(digitalPinToInterrupt(buttonPinNum[1]), secondLight, FALLING);
  //TODO interrupt on pin 4
  //TODO interrupt on pin 5
    //TODO create functions for each interrupt
 }
 
-void GetInactionTime() // move below GetAnswers()
+void loop()
 {
-  totalTime = millis();
-  currentTime = totalTime - previousTime;
-  previousTime = totalTime;
-
-  if(currentTime >= 300000)
+  bool gameMode = true;
+  while (gameMode == true)
   {
-    questionIsCorrect = false;
-    level = -1;
-    isSequenceCorrect = false;
-    gameMode = false;
+          //noInterrupts();
+  int score = 0;
+  bool isSequenceCorrect = true; // can you keep going?
+  for (int level = 1; level != -1; ++level) //level != -1 && isSequenceCorrect == true;
+  {
+          noInterrupts();
+    SetLightOrder();
+   isSequenceCorrect = false;
+          interrupts();
+    GetAnswers();
+  }
+  Serial.print("Score: ");
+  //PlayAgain();
   }
 }
 
@@ -59,7 +65,6 @@ void SetLightOrder()
 
 void GetAnswers()
 {
-  isSequenceCorrect = false; // move to loop() under call for SetLightOrder
   bool questionIsCorrect = false;
   for (int i = 0; i < level;)
   {
@@ -73,28 +78,34 @@ void GetAnswers()
     ++score;
   }
 }
+                 
+void GetInactionTime() // move below GetAnswers()
+{
+  totalTime = millis();
+  currentTime = totalTime - previousTime;
+  previousTime = totalTime;
+
+  if(currentTime >= 300000)
+  {
+    questionIsCorrect = false;
+    level = -1;
+    isSequenceCorrect = false;
+    gameMode = false;
+  }
+}
 
 void PlayAgain()
 {
   //check an input, if true then gameMode = true; else gameMode = false;
 }
-
-void loop()
+                 
+void firstLight() //interrupt 0 = pin 2 match up with led pin 10
 {
-  bool gameMode = true;
-  while (gameMode == true)
-  {
-          //noInterrupts();
-  int score = 0;
-  bool isSequenceCorrect = true; // can you keep going?
-  for (int level = 1; level != -1; ++level) //level != -1 && isSequenceCorrect == true;
-  {
-          noInterrupts();
-    SetLightOrder();
-          interrupts();
-    GetAnswers();
-  }
-  Serial.print("Score: ");
-  //PlayAgain();
-  }
+ LedPinNum[switchOrder[i]]
+ {
+  questionIsCorrect = true;
+  ++i;
 }
+}
+                 
+
